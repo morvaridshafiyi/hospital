1eb3717db2f98a3e46cc760cead488beeb0a3006
مروارید شفیعی <morvarid.shafiei@mahan.aero>
2023-12-13 14:33:13 +0330

    #f.. switch value

--- src/components/elements/switch/index.js
+++ src/components/elements/switch/index.js
@@ -1,8 +1,10 @@
+import { useState } from "react";
 import "./style.scss";
 const Switch = ({label}) => {
+  const [value , setValue] =useState(false)
   return (
     <div className="switch-container">
-      <label className="switch">
+      <label className="switch" onClick={()=>setValue(!value)}>
         <input type="checkbox" /> <div></div>
       </label>
       <span className="title">{label}</span>
